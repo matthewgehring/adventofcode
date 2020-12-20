06d188c43c4867be372757bb7a096a4f1b39946f
unknown <mgehring9@gmail.com>
2020-12-19 21:05:09 -0800

    part 1 and 2 but you have to change stuff for either lol

--- 2020/day19/script.js
+++ 2020/day19/script.js
@@ -1,7 +1,7 @@
 import fs from 'fs';
 
 fs.readFile('./data.txt', 'utf8', (err, data) => {
-    //change precendence for part1/part2
+
     console.log('question: ', main(data.split('\r\n\r\n')));
 })
 
@@ -12,8 +12,8 @@ const main = data => {
         let [key, value] = rule.split(':');
         ruleMap.set(key, value.trim().replace(/"/g, ''));
     });
-    let exp = regExBuilder(ruleMap, ruleMap.get('0').split(''));
-    console.log(exp);
+    let exp = regExBuilder(ruleMap, ruleMap.get('0').split(' '));
+    exp = exp.split(' ').join('');
     let reg = new RegExp(exp, 'g');
     let counter = 0;
     strings.split('\r\n').forEach(string => {
@@ -24,11 +24,13 @@ const main = data => {
     return counter;
 }
 
-const regExBuilder = (ruleMap, arr) => {
-    let nums = new RegExp('[0-9]+', 'g');
-    let ops = new RegExp('[\(|\)|\|]', 'g')
+const regExBuilder = (ruleMap, arr, count = 0) => {
+    let num = new RegExp('[0-9]+', 'g');
+    let nums = new RegExp('^[0-9]+$', 'g');
+    let ops = new RegExp('[\(|\)|\|]', 'g');
 
-    if(!(arr.join('').match(nums))){
+    if(!(arr.join('').match(num))){
+        console.log(arr.length);
         let expression = '^'+arr.join('') + '$';
         return expression;
     }
@@ -37,19 +39,28 @@ const regExBuilder = (ruleMap, arr) => {
     
     for(let x=0; x<arr.length; x++){
         if(arr[x].match(nums)){
-            let value = arr[x].match(nums)[0];
-            if(ruleMap.get(value).match(nums)){
-                answer.push(`(${ruleMap.get(value)})`);
-            } else{
-                answer.push(ruleMap.get(value));
+            if((arr[x] === '8' || arr[x]=== '11') && count < 10){
+                let value = arr[x].match(nums)[0];
+                if(ruleMap.get(value).match(num)){
+                    answer.push(`( ${ruleMap.get(value)} )`);
+                } else{
+                    answer.push(ruleMap.get(value));
+                }
+            } else if (arr[x] === '8' || arr[x]=== '11'){
+                continue;
+            } else {
+                let value = arr[x].match(nums)[0];
+                if(ruleMap.get(value).match(num)){
+                    answer.push(`( ${ruleMap.get(value)} )`);
+                } else{
+                    answer.push(ruleMap.get(value));
+                }
             }
         } else if(arr[x] === '(' || arr[x]===')'|| arr[x]==='|' || arr[x]==='a'|| arr[x]==='b'){
             answer.push(arr[x])
         }
-        //answer.push(arr[x]);
     }
-
-//regExBuilder(ruleMap, answer.join('').split(''));
-    return regExBuilder(ruleMap, answer.join('').split(''));
+    
+    return regExBuilder(ruleMap, answer.join(' ').split(' '), count + 1);
 
 }
